134953126ce9dfba2ca9020dc7248579509ea4f5
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-12-08 18:11:50 +0100

    Adds header and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { CartComponent } from './components/cart/cart.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { HeaderComponent } from './header/header.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, MoviesComponent, CartComponent, AdminComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent,
+    MoviesComponent,
+    CartComponent,
+    AdminComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: MoviesComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'admin', component: AdminComponent },
+      { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
